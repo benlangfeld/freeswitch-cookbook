8f876444fc74932f30d254f6611aecb62956cb2c
Ben Langfeld <ben@langfeld.me>
2013-10-13 11:51:12 -0300

    Verify that FS gets installed, fs_cli is functional and SIP/XMPP(Rayo) ports are listening

--- test/integration/package/serverspec/localhost/freeswitch_spec.rb
+++ test/integration/package/serverspec/localhost/freeswitch_spec.rb
@@ -10,4 +10,21 @@ describe 'FreeSWITCH' do
     it { should be_enabled }
     it { should be_running }
   end
+
+  describe command('freeswitch -version') do
+    it { should return_stdout /FreeSWITCH version/ }
+  end
+
+  describe command('fs_cli -x "version"') do
+    it { should return_stdout /FreeSWITCH Version 1.2/ }
+  end
+
+  describe port(5060) do
+    it { should be_listening.with('tcp') }
+    it { should be_listening.with('udp') }
+  end
+
+  describe port(5222) do
+    it { should be_listening.with('tcp') }
+  end
 end
