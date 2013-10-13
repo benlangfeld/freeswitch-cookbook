f8dffcaa088ccc18a3355cfffd632c4dd1f52b6d
Ben Langfeld <ben@langfeld.me>
2013-10-13 15:57:22 -0300

    Properly capitalise FreeSWITCH

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "freeswitch"
 maintainer       "Mojo Lingo LLC"
 maintainer_email "ops@mojolingo.com"
 license          "Apache 2.0"
-description      "Installs/Configures freeswitch"
+description      "Installs/Configures FreeSWITCH"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 
