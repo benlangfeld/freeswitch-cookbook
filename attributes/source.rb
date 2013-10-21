f77b8f0b316e6230fd5301a08b659430af67932e
Ben Langfeld <ben@langfeld.me>
2013-10-21 10:45:27 -0200

    Remove unused attribute

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,6 +1,5 @@
 default['freeswitch']['source']['git_uri'] = "git://git.freeswitch.org/freeswitch.git"
 default['freeswitch']['source']['git_branch'] = "v1.2.stable"
-default['freeswitch']['source']['enabled'] = "true"
 default['freeswitch']['source']['dependencies'] = %w[
 autoconf
 automake
