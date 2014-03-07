863be327fec84cfc096bac941c9f96d24b2a3cb8
Ben Langfeld <ben@langfeld.me>
2014-03-06 22:14:42 -0300

    Bump version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "ops@mojolingo.com"
 license          "Apache 2.0"
 description      "Installs/Configures FreeSWITCH"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.2"
+version          "0.4.0"
 
 recipe "freeswitch", "Install FreeSWITCH and configure"
 recipe "freeswitch::package", "Install FreeSWITCH from a package"
